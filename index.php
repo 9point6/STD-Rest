e3c0e3e2ea7e99cce9edd8d2bc700c2ffaadea6b
Richard Lyon <richthegeek@gmail.com>
2011-08-06 01:18:10 +0100

    Making index.php and index.html show the same thing

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 require_once("php/class.apifactory.php");
 
-$s = microtime(true);
-
 $api = new APIFactory();
 $api->fromJSON("./apis/mobicart.json");
+
+$api->api_key = "lololol";
+$api->user_name = "richthegeek@gmail.com";
+$api->store->get_tax(4525);
